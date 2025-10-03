e4d7fd909b5ff91c67fd44aa569d8080961f4d3e
faddwaaa <fadwapamulasih@gmail.com>
2025-10-03 07:56:24 +0700

    menambahkan file mvc02

---
+++ MVC/app/core/Database.php
@@ -0,0 +1,12 @@
+<?php
+class Database {
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $db_name = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    
+}
+?>
---
+++ MVC02/app/views/templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman <?= $data['judul']; ?></title>
+    <link rel="stylesheet" href="http://http://localhost/PemWeb/MVC02/public/css/bootstrap.css">
+</head>
+<body>
